b4e26a36b26d3f8ebde17be5ce07561280b072c2
vikanshu-98 <vikanshu8954985003@gmail.com>
2022-06-22 18:27:41 +0530

    fixed issue when user is not login

--- app/config/passport.js
+++ app/config/passport.js
@@ -6,16 +6,16 @@ function intializePassport(passport){
     passport.use(new Passportlocal({usernameField:'email'},async function(email,password,done){
         const user = await users.findOne({email:email});
         if(!user){
-            return done(null,false,{message:"No user with this email"})
+            return done(null,false,{message:"*No user with this email"})
         }
         bcrypt.compare(password,user.password,function(err,correct){
             if(err){
-                return done(null,false,{message:"somethins went wrong!! "})
+                return done(null,false,{message:"*somethins went wrong!! "})
             }
             if(correct){
-                return done(null,user,{message:"Logged in successfully"})
+                return done(null,user,{message:"*Logged in successfully"})
             }
-            return done(null,false,{message:"wrong username or password"})
+            return done(null,false,{message:"*wrong username or password"})
         })
 
     }))
