05a2ebcf63ee439ddec5646dbd2c6b94bd341937
vikanshu-98 <vikanshu8954985003@gmail.com>
2022-06-22 15:44:30 +0530

    update connection

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,8 +1,10 @@
 
 require('dotenv').config()
-
+const dbOptions = { 
+    connectTimeoutMS: 10000, 
+};
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_SECRET_KEY)  
+mongoose.connect(process.env.DB_SECRET_KEY,dbOptions)  
 .then(()=>{console.log('connect successfully');})
 .catch((err)=>{console.log(err);})
 
